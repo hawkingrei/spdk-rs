814bf7338eddd06ff1bef90669ee247f188530e3
Wangweizhen <hawking.rei@gmail.com>
2018-10-14 01:26:03 +0800

    update

--- example/nvme/hello_world/src/main.rs
+++ example/nvme/hello_world/src/main.rs
@@ -1,6 +1,22 @@
 extern crate spdk_sys;
 
 use spdk_sys::*;
+use std::ptr;
+struct ctrlr_entry {
+    ctrlr: *mut spdk_nvme_ctrlr,
+    next: *mut ctrlr_entry,
+    name: [u8; 1024],
+}
+
+struct ns_entry {
+    ctrlr: *mut spdk_nvme_ctrlr,
+    ns: *mut spdk_nvme_ns,
+    next: *mut ns_entry,
+    qpair: *mut spdk_nvme_qpair,
+}
+
+static g_controllers: *mut ctrlr_entry = ptr::null_mut();
+static g_namespaces: *mut ns_entry = ptr::null_mut();
 
 fn main() {
     let opt: *mut spdk_env_opts = &mut Default::default();
