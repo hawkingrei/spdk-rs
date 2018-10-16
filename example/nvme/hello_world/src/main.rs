dd6c3c7df26be168b4615596e66e4ba23a990e5a
Wangweizhen <hawking.rei@gmail.com>
2018-10-16 22:12:46 +0800

    update

--- example/nvme/hello_world/src/main.rs
+++ example/nvme/hello_world/src/main.rs
@@ -1,7 +1,9 @@
 extern crate spdk_sys;
 
 use spdk_sys::*;
+use std::process;
 use std::ptr;
+
 struct ctrlr_entry {
     ctrlr: *mut spdk_nvme_ctrlr,
     next: *mut ctrlr_entry,
@@ -24,7 +26,27 @@ unsafe impl Sync for ns_entry {}
 fn main() {
     let opt: *mut spdk_env_opts = &mut Default::default();
     unsafe {
+        /*
+         * SPDK relies on an abstraction around the local environment
+         * named env that handles memory allocation and PCI device operations.
+         * This library must be initialized first.
+         *
+         */
         spdk_env_opts_init(opt);
+        opts.name = "hello_world";
+        opts.shm_id = 0;
+        if (spdk_env_init(opts) < 0) {
+            println!("Initializing NVMe Controllers\n");
+            process::exit(1);
+        }
+        println!("Initializing NVMe Controllers\n");
+        /*
+         * Start the SPDK NVMe enumeration process.  probe_cb will be called
+         *  for each NVMe controller found, giving our application a choice on
+         *  whether to attach to each controller.  attach_cb will then be
+         *  called for each controller after the SPDK NVMe driver has completed
+         *  initializing the controller we chose to attach.
+         */
+        //let rc = rc = spdk_nvme_probe(ptr::null(), ptr::null(), probe_cb, attach_cb, ptr::null());
     }
-    println!("Hello, world!");
 }
