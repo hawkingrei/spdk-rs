bba550c57e96bb97dd7fb6f93b85e33c76ccaf4e
Wangweizhen <hawking.rei@gmail.com>
2018-10-17 14:00:34 +0800

    update

--- example/nvme/hello_world/src/main.rs
+++ example/nvme/hello_world/src/main.rs
@@ -5,7 +5,6 @@ use spdk_sys::*;
 use std::ffi::CString;
 use std::process;
 use std::ptr;
-use libc;
 
 struct ctrlr_entry {
     ctrlr: *mut spdk_nvme_ctrlr,
@@ -31,8 +30,34 @@ fn probe_cb(
     trid: *const spdk_nvme_transport_id,
     opts: *mut spdk_nvme_ctrlr_opts,
 ) -> bool {
-    println!("{:?}", (*trid).traddr);
+    println!("{:?}", escape((*trid).traddr));
     return true;
+}
+
+pub fn escape(data: &[u8]) -> String {
+    let mut escaped = Vec::with_capacity(data.len() * 4);
+    for &c in data {
+        match c {
+            b'\n' => escaped.extend_from_slice(br"\n"),
+            b'\r' => escaped.extend_from_slice(br"\r"),
+            b'\t' => escaped.extend_from_slice(br"\t"),
+            b'"' => escaped.extend_from_slice(b"\\\""),
+            b'\\' => escaped.extend_from_slice(br"\\"),
+            _ => {
+                if c >= 0x20 && c < 0x7f {
+                    // c is printable
+                    escaped.push(c);
+                } else {
+                    escaped.push(b'\\');
+                    escaped.push(b'0' + (c >> 6));
+                    escaped.push(b'0' + ((c >> 3) & 7));
+                    escaped.push(b'0' + (c & 7));
+                }
+            }
+        }
+    }
+    escaped.shrink_to_fit();
+    unsafe { String::from_utf8_unchecked(escaped) }
 }
 
 fn main() {
