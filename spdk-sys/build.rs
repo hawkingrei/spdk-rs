f17878aaa40efe36e6bdbeca86f6f9fe5e693b7c
root <root@vagrant.vm>
2019-02-06 09:05:23 +0000

    u

--- spdk-sys/build.rs
+++ spdk-sys/build.rs
@@ -6,74 +6,8 @@ use std::path::PathBuf;
 
 static SPDK_INCLUDE_DIR: &'static str = "/usr/local/include";
 
-fn generate_bindings() {
-    let spdk_include_path = env::var("SPDK_INCLUDE").unwrap_or(SPDK_INCLUDE_DIR.to_string());
-    //let output_path = env::var("OUT_DIR").unwrap();
-    let output_path = "./src/";
-    let generator = Generator {
-        spdk_include_path: Path::new(&spdk_include_path),
-        output_path: Path::new(&output_path),
-    };
-
-    let headers = [
-        "nvme",
-        "event",
-        "bdev",
-        "env",
-        "blob_bdev",
-        "blob",
-        "log",
-        "io_channel",
-    ];
-    generator.generate(&headers)
-}
-
-struct Generator<'a> {
-    spdk_include_path: &'a Path,
-    output_path: &'a Path,
-}
-
-impl<'a> Generator<'a> {
-    fn generate(&self, names: &[&str]) {
-        let mut codegen_config = bindgen::CodegenConfig::empty();
-        codegen_config.set(bindgen::CodegenConfig::FUNCTIONS, true);
-        codegen_config.set(bindgen::CodegenConfig::TYPES, true);
-        codegen_config.set(bindgen::CodegenConfig::CONSTRUCTORS, true);
-        codegen_config.set(bindgen::CodegenConfig::METHODS, true);
-
-        let mut builder = bindgen::builder();
-
-        for name in names {
-            let header_path = self.spdk_include_path.join(
-                PathBuf::from("spdk/header.h")
-                    .with_file_name(name)
-                    .with_extension("h"),
-            );
-            builder = builder.header(format!("{}", header_path.display()));
-        }
-
-        let bindings = builder
-            .derive_default(true)
-            .with_codegen_config(codegen_config)
-            .generate_inline_functions(false)
-            // If there are linking errors and the generated bindings have weird looking
-            // #link_names (that start with \u{1}), the make sure to flip that to false.
-            .trust_clang_mangling(false)
-            .rustfmt_bindings(true)
-            .rustfmt_configuration_file(Some(PathBuf::from("../rustfmt.toml")))
-            .layout_tests(false)
-            .ctypes_prefix("libc")
-            .generate()
-            .expect("Unable to generate bindings");
-
-        bindings
-            .write_to_file(self.output_path.join("spdk_bindings.rs"))
-            .expect("Couldn't write bindings!");
-    }
-}
-
 fn main() {
-    generate_bindings();
+    let spdk_include_path = env::var("SPDK_INCLUDE").unwrap_or(SPDK_INCLUDE_DIR.to_string());
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-link-lib=spdk");
     println!("cargo:rustc-link-search=native=/usr/local/lib");
@@ -82,4 +16,33 @@ fn main() {
     println!("cargo:rustc-link-search=native=/usr/local/include/spdk");
     println!("cargo:rustc-link-search=native=/usr/include/");
     println!("cargo:rustc-link-search=native=/usr/local/lib");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        .clang_arg(spdk_include_path)
+        // The input header we would like to generate
+        // bindings for.
+        .derive_default(true)
+        .header("wrapper.h")
+        .blacklist_type("IPPORT_.*")   // https://github.com/rust-lang-nursery/rust-bindgen/issues/687
+        .blacklist_type("max_align_t") // https://github.com/rust-lang-nursery/rust-bindgen/issues/550
+        .opaque_type("spdk_nvme_feat_async_event_configuration") // https://github.com/rust-lang-nursery/rust-bindgen/issues/687
+        .opaque_type("spdk_nvme_feat_async_event_configuration__bindgen_ty_1")
+        // If there are linking errors and the generated bindings have weird looking
+        // #link_names (that start with \u{1}), the make sure to flip that to false.
+        .trust_clang_mangling(false)
+        .rustfmt_bindings(true)
+        .rustfmt_configuration_file(Some(PathBuf::from("../rustfmt.toml")))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
