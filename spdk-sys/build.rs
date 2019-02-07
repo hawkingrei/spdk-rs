7a35b6d392c35028f58e2f6607f0ef5257059e7c
Wangweizhen <wangweizhen@bilibili.com>
2019-02-07 15:07:39 +0000

    u

--- spdk-sys/build.rs
+++ spdk-sys/build.rs
@@ -6,126 +6,8 @@ use std::path::PathBuf;
 
 static SPDK_INCLUDE_DIR: &'static str = "/usr/local/include";
 
-fn generate_bindings() {
-    let spdk_include_path = env::var("SPDK_INCLUDE").unwrap_or(SPDK_INCLUDE_DIR.to_string());
-    //let output_path = env::var("OUT_DIR").unwrap();
-    let output_path = "./src/";
-    let generator = Generator {
-        spdk_include_path: Path::new(&spdk_include_path),
-        output_path: Path::new(&output_path),
-    };
-
-    let headers = [
-		"assert",
-		"barrier",
-		"base64",
-		"bdev",
-		"bdev_module",
-		"bit_array",
-		"blob_bdev",
-		"blobfs",
-		"blob",
-		"conf",
-		"config",
-		"copy_engine",
-		"cpuset",
-		"crc16",
-		"crc32",
-		"dif",
-		"endian",
-		"env_dpdk",
-		"env",
-		"event",
-		"fd",
-		"ftl",
-		"gpt_spec",
-		"histogram_data",
-		"ioat",
-		"ioat_spec",
-		"io_channel",
-		"iscsi_spec",
-		"json",
-		"jsonrpc",
-		"likely",
-		"log",
-		"lvol",
-		"mmio",
-		"nbd",
-		"net",
-		"nvme",
-		"nvme_intel",
-		"nvme_ocssd",
-		"nvme_ocssd_spec",
-		"nvme_spec",
-		"nvmf_fc_spec",
-		"nvmf",
-		"nvmf_spec",
-		"pci_ids",
-		"queue_extras",
-		"queue",
-		"reduce",
-		"rpc",
-		"scsi",
-		"scsi_spec",
-		"sock",
-		"stdinc",
-		"string",
-		"thread",
-		"trace",
-		"util",
-		"uuid",
-		"version",
-		"vhost",
-    ];
-    generator.generate(&headers)
-}
-
-struct Generator<'a> {
-    spdk_include_path: &'a Path,
-    output_path: &'a Path,
-}
-
-impl<'a> Generator<'a> {
-    fn generate(&self, names: &[&str]) {
-        let mut codegen_config = bindgen::CodegenConfig::empty();
-        codegen_config.set(bindgen::CodegenConfig::FUNCTIONS, true);
-        codegen_config.set(bindgen::CodegenConfig::TYPES, true);
-        codegen_config.set(bindgen::CodegenConfig::CONSTRUCTORS, true);
-        codegen_config.set(bindgen::CodegenConfig::METHODS, true);
-
-        let mut builder = bindgen::builder();
-
-        for name in names {
-            let header_path = self.spdk_include_path.join(
-                PathBuf::from("spdk/header.h")
-                    .with_file_name(name)
-                    .with_extension("h"),
-            );
-            builder = builder.header(format!("{}", header_path.display()));
-        }
-
-        let bindings = builder
-            .derive_default(true)
-            .with_codegen_config(codegen_config)
-            .generate_inline_functions(false)
-            // If there are linking errors and the generated bindings have weird looking
-            // #link_names (that start with \u{1}), the make sure to flip that to false.
-            .trust_clang_mangling(false)
-            .rustfmt_bindings(true)
-            .rustfmt_configuration_file(Some(PathBuf::from("../rustfmt.toml")))
-            .layout_tests(false)
-            .ctypes_prefix("libc")
-            .generate()
-            .expect("Unable to generate bindings");
-
-        bindings
-            .write_to_file(self.output_path.join("spdk_bindings.rs"))
-            .expect("Couldn't write bindings!");
-    }
-}
-
 fn main() {
-    generate_bindings();
+    let spdk_include_path = env::var("SPDK_INCLUDE").unwrap_or(SPDK_INCLUDE_DIR.to_string());
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-link-lib=spdk");
     println!("cargo:rustc-link-search=native=/usr/local/lib");
@@ -134,4 +16,46 @@ fn main() {
     println!("cargo:rustc-link-search=native=/usr/local/include/spdk");
     println!("cargo:rustc-link-search=native=/usr/include/");
     println!("cargo:rustc-link-search=native=/usr/local/lib");
+
+    let mut codegen_config = bindgen::CodegenConfig::empty();
+        codegen_config.set(bindgen::CodegenConfig::FUNCTIONS, true);
+        codegen_config.set(bindgen::CodegenConfig::TYPES, true);
+        codegen_config.set(bindgen::CodegenConfig::CONSTRUCTORS, true);
+    codegen_config.set(bindgen::CodegenConfig::METHODS, true);
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        .derive_default(true)
+        .header("wrapper.h")
+        .with_codegen_config(codegen_config)
+        .generate_inline_functions(false)
+            // If there are linking errors and the generated bindings have weird looking
+            // #link_names (that start with \u{1}), the make sure to flip that to false.
+        .trust_clang_mangling(false)
+        .rustfmt_bindings(true)
+        .rustfmt_configuration_file(Some(PathBuf::from("../rustfmt.toml")))
+        .layout_tests(false)
+        .ctypes_prefix("libc")
+
+        .clang_arg(spdk_include_path)
+        // The input header we would like to generate
+        // bindings for.
+        .derive_default(true)
+        .blacklist_type("IPPORT_.*")   // https://github.com/rust-lang-nursery/rust-bindgen/issues/687
+        .blacklist_type("max_align_t") // https://github.com/rust-lang-nursery/rust-bindgen/issues/550
+        .opaque_type("spdk_nvme_feat_async_event_configuration") // https://github.com/rust-lang-nursery/rust-bindgen/issues/687
+        .opaque_type("spdk_nvme_feat_async_event_configuration__bindgen_ty_1")
+        // If there are linking errors and the generated bindings have weird looking
+        // #link_names (that start with \u{1}), the make sure to flip that to false.
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
