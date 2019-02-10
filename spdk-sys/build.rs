5fd802a054102ebe1f98ba22ebfc3b157250ff54
root <root@vagrant.vm>
2019-02-10 05:10:51 +0000

    u

--- spdk-sys/build.rs
+++ spdk-sys/build.rs
@@ -11,22 +11,27 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-link-lib=spdk");
     println!("cargo:rustc-link-search=native=/usr/local/lib");
-
+    println!("cargo:rustc-link-search=native=/usr/local/include");
     let mut codegen_config = bindgen::CodegenConfig::empty();
-        codegen_config.set(bindgen::CodegenConfig::FUNCTIONS, true);
-        codegen_config.set(bindgen::CodegenConfig::TYPES, true);
-        codegen_config.set(bindgen::CodegenConfig::CONSTRUCTORS, true);
+    codegen_config.set(bindgen::CodegenConfig::FUNCTIONS, true);
+    codegen_config.set(bindgen::CodegenConfig::TYPES, true);
+    codegen_config.set(bindgen::CodegenConfig::CONSTRUCTORS, true);
     codegen_config.set(bindgen::CodegenConfig::METHODS, true);
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
     let bindings = bindgen::Builder::default()
+        .clang_arg(spdk_include_path)
         .derive_default(true)
         .header("wrapper.h")
         .with_codegen_config(codegen_config)
         .generate_inline_functions(false)
-            // If there are linking errors and the generated bindings have weird looking
-            // #link_names (that start with \u{1}), the make sure to flip that to false.
+        .blacklist_type("IPPORT_.*") // https://github.com/rust-lang-nursery/rust-bindgen/issues/687
+        .blacklist_type("max_align_t") // https://github.com/rust-lang-nursery/rust-bindgen/issues/550
+        .opaque_type("spdk_nvme_feat_async_event_configuration") // https://github.com/rust-lang-nursery/rust-bindgen/issues/687
+        .opaque_type("spdk_nvme_feat_async_event_configuration__bindgen_ty_1")
+        // If there are linking errors and the generated bindings have weird looking
+        // #link_names (that start with \u{1}), the make sure to flip that to false.
         .trust_clang_mangling(false)
         .rustfmt_bindings(true)
         .rustfmt_configuration_file(Some(PathBuf::from("../rustfmt.toml")))
@@ -35,7 +40,7 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .derive_default(true)
-        .blacklist_type("IPPORT_.*")   // https://github.com/rust-lang-nursery/rust-bindgen/issues/687
+        .blacklist_type("IPPORT_.*") // https://github.com/rust-lang-nursery/rust-bindgen/issues/687
         .blacklist_type("max_align_t") // https://github.com/rust-lang-nursery/rust-bindgen/issues/550
         .opaque_type("spdk_nvme_feat_async_event_configuration") // https://github.com/rust-lang-nursery/rust-bindgen/issues/687
         .opaque_type("spdk_nvme_feat_async_event_configuration__bindgen_ty_1")
