811dfcf7c438b2a90d7e40257199b1cdbc2f06a1
Weizhen Wang <hawking.rei@gmail.com>
2019-02-10 13:17:39 +0800

    Refactor bindgen (#4)
    * chore: update cargo

--- example/nvme/hello_world/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rustc-link-lib=spdk");
-    println!("cargo:rustc-link-search=native=/usr/local/lib");
-    println!("cargo:rustc-link-search=native=/usr/local/include");
-}
